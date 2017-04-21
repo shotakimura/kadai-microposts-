8a47b7e8d5f1e85cb84fe245b38d282b92b4fe0e
Shota Kimura <shotakimura0319@gmail.com>
2017-04-21 08:04:43 +0000

    follow, unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+
   def index
     @users = User.all.page(params[:page])
   end
@@ -12,11 +13,10 @@ before_action :require_user_logged_in, only: [:index, :show]
 
   def new
     @user = User.new
-    
   end
 
   def create
-     @user = User.new(user_params)
+    @user = User.new(user_params)
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
@@ -26,8 +26,20 @@ before_action :require_user_logged_in, only: [:index, :show]
       render :new
     end
   end
+
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
   
-   private
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
+
+  private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
